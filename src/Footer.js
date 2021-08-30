da6cc6b5e9071b27b5e6536b0c594f93472ef56c
JB-Tellez <jb.tellez@gmail.com>
2021-08-29 21:34:45 -0700

    cleans up css imprts

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 
 class Footer extends React.Component {
   render() {
-    return(
+    return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand>Code Fellows</Navbar.Brand>
+        <Navbar.Brand>Code Fellows</Navbar.Brand>
       </Navbar>
     )
   }
